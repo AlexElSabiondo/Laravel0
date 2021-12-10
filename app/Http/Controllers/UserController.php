e70c93891698fe7853cceb4c6f08cb8eefb5d968
AlexElSabiondo <alexingenierocierva@gmail.com>
2021-12-10 20:58:09 +0100

    Ejercicio 4 terminado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
                     $query->doesntHave('team');
                 }
             })
-            ->filterBy($userFilter, request()->only(['state', 'role', 'search', 'skills', 'from', 'to']))
+            ->filterBy($userFilter, request()->only(['state', 'role', 'search', 'skills', 'from', 'to', 'team']))
             ->orderBy('created_at', 'DESC')
             ->paginate();
 
